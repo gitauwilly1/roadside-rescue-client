3126aea641f21e6d423b67c0766a2a17fbf5dca9
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-04-01 17:22:43 +0300

    Delete duplicate import in register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { signOutGoogle } from '../config/firebase';
-import { useAuth } from '../context/AuthContext';
 
 const RegisterPage = () => {
   const location = useLocation();
